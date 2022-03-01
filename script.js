d53fe49f9948fb4a31ee47c3a724f4d1d16353f2
Can Tam <60790925+zMaax-source@users.noreply.github.com>
2022-03-01 23:16:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const targetDate = new Date('18 June 2022');
+
+
+function handleTimer(){
+    const date = new Date();
+    const timeDiff = targetDate.getTime() - date.getTime();
+    const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+}
+
+setInterval(handleTimer, 1000);
